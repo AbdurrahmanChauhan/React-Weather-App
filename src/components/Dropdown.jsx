d0346d82f6f874aaa148e2804d530848f829a3d8
khasheshaikh <khasheshaikh@gmail.com>
2022-01-15 14:37:47 +0530

    added css

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Form } from 'react-bootstrap'
+import { Button, Form,FormControl,InputGroup } from 'react-bootstrap'
 import CityName from './CityName'
 
 function Dropdown() {
@@ -30,23 +30,26 @@ function Dropdown() {
         </Button>
       </InputGroup>
       
-//       <div className="Dropdown">
-//         <Form.Select
-//           aria-label="Default select example"
-//           className="form-select"
-//           onChange={handleChange}
-//         >
-//           <option value="Mumbai">Mumbai</option>
-//           <option value="Pune">Pune</option>
-//           <option value="Nashik">Nashik</option>
-//         </Form.Select>
-//         <span>
-//           <Button onClick={handleClick} variant="outline-success">
-//             Fetch
-//           </Button>{' '}
-//         </span>
-      </div>
+       <div className="Dropdown">
+         <Form.Select
+          aria-label="Default select example"
+           className="form-select"
+           onChange={handleChange}
+         >
+           <option value="Mumbai">Mumbai</option>
+          <option value="Pune">Pune</option>
+           <option value="Nashik">Nashik</option>
+         </Form.Select>
+         <span>
+          <Button onClick={handleClick} variant="outline-success">
+          Fetch
+           </Button>{' '}
+         </span>
+
+     </div>
+
       <CityName city={city}/>
+
     </div>
   )
 }
